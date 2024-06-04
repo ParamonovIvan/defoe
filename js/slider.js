ea013d15b2b00afbbf960e02642b17429a431861
Ivan Paramonov <par-ivan@mail.ru>
2024-06-04 15:26:17 +0400

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,41 @@
-﻿document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
+
   const showSlider = new Swiper('.showcase-carousel', {
+
     loop: true,
-    slidesPerView: 3,
+
+    slidesPerView: 1,
+
     speed: 1500,
+
     centeredSlides: true,
+
     pagination: {
+
       el: '.showcase-pagination',
+
       type: 'fraction',
+
     },
+
     navigation: {
+
       nextEl: '.showcase-navigation__next',
+
       prevEl: '.showcase-navigation__prev'
+
     },
-  })
+
+    breakpoints: {
+
+      640: {
+
+        slidesPerView: 3
+
+      },
+
+    },
+
+  });
+
 });
